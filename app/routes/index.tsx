dadb582a19d80790ab22cd714324a0e7d3dd51f3
maguro-alternative <ka.ra.si.naporitan@gmail.com>
2025-07-05 08:40:16 +0900

    一旦トップ作成

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,5 +1,7 @@
 import { createRoute } from "honox/factory";
 import Twitter from '../components/card/twitter';
+import Github from "../components/card/github";
+import Uchuemon from "../components/icon/uchuemon";
 
 import SpotlightAndWave from '../islands/SpotLightAndWave';
 
@@ -7,7 +9,24 @@ export default createRoute(async (c) => {
   return c.render(
     <div className="container">
       <SpotlightAndWave />
-      <Twitter id="sigumataityouda" />
+      <div className="header" style={{ display: 'flex', justifyContent: 'end', alignItems: 'center', padding: '10px' }}>
+        <Twitter id="sigumataityouda" />
+        <Github id="maguro-alternative" />
+      </div>
+      <div className="card" style={{ color: 'white' }}>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginBottom: '20px' }}>
+          <Uchuemon />
+        </div>
+        <div style={{ textAlign: 'center' }}>
+          <h1>ユニコォォォォォォォォォォォォォォォォォーーーーーーーーーン！！！</h1>
+          <p>
+            This is a sample application built with Honox, showcasing the use of islands and components.
+          </p>
+          <p>
+            Explore the code and see how you can create interactive web applications with ease!
+          </p>
+          </div>
+      </div>
     </div>
   );
 });
