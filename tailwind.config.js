8f997f8bbcf32d874513cf68e2369fc0f32e7de9
maguro-alternative <ka.ra.si.naporitan@gmail.com>
2025-07-18 07:36:18 +0900

    tailwind導入

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './app/**/*.tsx',
+    './app/routes/**/*.{ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
