3ab8946f0bb7cc754dc650197e563640c84e17d7
maguro-alternative <ka.ra.si.naporitan@gmail.com>
2025-07-29 00:01:59 +0900

    字下げ

--- app/layout/SpotLightShape.ts
+++ app/layout/SpotLightShape.ts
@@ -3,17 +3,17 @@ export function drawSpotLight(
   w: number,
   h: number
 ): void {
-    // 1個目の円
-    const dx: number = w / 3 + (w / 10) * Math.sin(Date.now() / 4000);
-    const dy1: number = h / 3;
-    const size1: number = w / 2;
-    drawCircle(context, dx, dy1, size1, "rgba(255, 255, 255, 0.3)");
+  // 1個目の円
+  const dx: number = w / 3 + (w / 10) * Math.sin(Date.now() / 4000);
+  const dy1: number = h / 3;
+  const size1: number = w / 2;
+  drawCircle(context, dx, dy1, size1, "rgba(255, 255, 255, 0.3)");
 
-    // 2個目の円
-    const dx2: number = (w * 3) / 4 + (w / 15) * Math.cos(Date.now() / 10000);
-    const dy2: number = (h * 2) / 3;
-    const size2: number = w / 3;
-    drawCircle(context, dx2, dy2, size2, "rgba(255, 255, 255, 0.1)");
+  // 2個目の円
+  const dx2: number = (w * 3) / 4 + (w / 15) * Math.cos(Date.now() / 10000);
+  const dy2: number = (h * 2) / 3;
+  const size2: number = w / 3;
+  drawCircle(context, dx2, dy2, size2, "rgba(255, 255, 255, 0.1)");
 }
 
 function drawCircle(
