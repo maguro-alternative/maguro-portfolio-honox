0ae73e268bdf8bf39648e6a97684dcb0c3a13ec7
maguro-alternative <ka.ra.si.naporitan@gmail.com>
2025-08-11 09:11:29 +0900

    useLayoutEffect削除

--- app/islands/SpotLightAndWave.tsx
+++ app/islands/SpotLightAndWave.tsx
@@ -1,6 +1,6 @@
 import { drawWave } from "../layout/WaveShape";
 import { drawSpotLight } from "../layout/SpotLightShape";
-import { useRef, useLayoutEffect, useSyncExternalStore } from "hono/jsx";
+import { useRef, useLayoutEffect, useSyncExternalStore, type RefObject } from "hono/jsx";
 
 const isClient = typeof window !== 'undefined';
 
@@ -30,58 +30,64 @@ function useResizeEffect() {
   return useSyncExternalStore(resizeSubscribe, getSnapshot, getServerSnapshot);
 }
 
+function canvasSpotligth(
+  width: number,
+  height: number,
+  canvasWaveRef: RefObject<HTMLCanvasElement>,
+  canvasOverlayRef: RefObject<HTMLCanvasElement>
+) {
+  const canvasWave = canvasWaveRef.current;
+  const canvasOverlay = canvasOverlayRef.current;
+
+  if (!canvasWave || !canvasOverlay) return;
+
+  const contextWave = canvasWave.getContext("2d");
+  const contextOverlay = canvasOverlay.getContext("2d");
+
+  if (!contextWave || !contextOverlay) return;
+
+  canvasWave.width = width;
+  canvasWave.height = height;
+  canvasOverlay.width = width;
+  canvasOverlay.height = height;
+
+  if (width === 0 || height === 0) {
+    return;
+  }
+
+  let animationFrameId: number;
+  const tick = () => {
+    animationFrameId = requestAnimationFrame(tick);
+
+    contextWave.fillStyle = "rgba(0, 0, 0, 0.2)";
+    contextWave.fillRect(0, 0, canvasWave.width, canvasWave.height);
+
+    drawWave(contextWave, canvasWave.width, canvasWave.height);
+
+    contextOverlay.clearRect(0, 0, canvasOverlay.width, canvasOverlay.height);
+    drawSpotLight(contextOverlay, canvasOverlay.width, canvasOverlay.height);
+
+    contextOverlay.globalCompositeOperation = "lighter";
+    contextOverlay.drawImage(canvasWave, 0, 0);
+  };
+
+  tick();
+
+  return () => {
+    cancelAnimationFrame(animationFrameId);
+  };
+}
+
 export default function SpotlightAndWave() {
   const canvasWaveRef = useRef<HTMLCanvasElement>(null);
   const canvasOverlayRef = useRef<HTMLCanvasElement>(null);
   const { width, height } = useResizeEffect();
 
-  useLayoutEffect(() => {
-    const canvasWave = canvasWaveRef.current;
-    const canvasOverlay = canvasOverlayRef.current;
-
-    if (!canvasWave || !canvasOverlay) return;
-
-    const contextWave = canvasWave.getContext("2d");
-    const contextOverlay = canvasOverlay.getContext("2d");
-
-    if (!contextWave || !contextOverlay) return;
-
-    canvasWave.width = width;
-    canvasWave.height = height;
-    canvasOverlay.width = width;
-    canvasOverlay.height = height;
-
-    if (width === 0 || height === 0) {
-      return;
-    }
-
-    let animationFrameId: number;
-    const tick = () => {
-      animationFrameId = requestAnimationFrame(tick);
-
-      contextWave.fillStyle = "rgba(0, 0, 0, 0.2)";
-      contextWave.fillRect(0, 0, canvasWave.width, canvasWave.height);
-
-      drawWave(contextWave, canvasWave.width, canvasWave.height);
-
-      contextOverlay.clearRect(0, 0, canvasOverlay.width, canvasOverlay.height);
-      drawSpotLight(contextOverlay, canvasOverlay.width, canvasOverlay.height);
-
-      contextOverlay.globalCompositeOperation = "lighter";
-      contextOverlay.drawImage(canvasWave, 0, 0);
-    };
-
-    tick();
-
-    return () => {
-      cancelAnimationFrame(animationFrameId);
-    };
-  }, [canvasWaveRef, canvasOverlayRef, width, height]);
-
+  canvasSpotligth(width, height, canvasWaveRef, canvasOverlayRef)
   const canvasStyle = { position: "fixed", top: 0, left: 0, width: "100%", height: "100%" };
 
   return (
-    <>
+    <div>
       <div id="bg" style={{ ...canvasStyle, zIndex: -2, background: "linear-gradient(to bottom, hsl(145, 33%, 48%),hsl(152, 21%, 54%),hsl(175, 20%, 53%))", animation: "AnimationName 10s ease infinite" }}></div>
       <canvas
         id="canvasWave"
@@ -93,6 +99,6 @@ export default function SpotlightAndWave() {
         ref={canvasOverlayRef}
         style={{ ...canvasStyle, zIndex: -1, mixBlendMode: "hard-light" }}
       />
-    </>
+    </div>
   );
 };
