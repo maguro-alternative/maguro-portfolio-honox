3581e39d28875ce51024dede440d0d8e71d0070d
maguro-alternative <ka.ra.si.naporitan@gmail.com>
2025-08-19 07:04:58 +0900

    index

--- vite-node-server-plugin.ts
+++ vite-node-server-plugin.ts
@@ -49,7 +49,7 @@ export const nodeServerPlugin = (): Plugin => {
             external: [...builtinModules, /^node:/],
             input: virtualEntryId,
             output: {
-              entryFileNames: 'server.js',
+              entryFileNames: 'index.js',
             },
           },
         },
