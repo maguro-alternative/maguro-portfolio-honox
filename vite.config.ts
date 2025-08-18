9689804c33d725a93a24430a993a5ada5ec1f5a2
maguro-alternative <ka.ra.si.naporitan@gmail.com>
2025-08-19 08:15:38 +0900

    シンプルにできるかな？？？

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,12 @@
-import mdx from '@mdx-js/rollup'
+import vercel from '@hono/vite-build'
 import honox from 'honox/vite'
-import client from 'honox/vite/client'
+import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
-import nodeServerPlugin from './vite-node-server-plugin'
 
-export default defineConfig(({ mode }) => {
-  if (mode === 'client') {
-    return {
-      plugins: [client()],
-    }
-  } else {
-    return {
-      plugins: [
-        honox(),
-        nodeServerPlugin(),
-        mdx({ jsxImportSource: 'hono/jsx' }),
-      ],
-    }
-  }
+export default defineConfig({
+  plugins: [
+    honox(),
+    tailwindcss(),
+    vercel({})
+  ]
 })
