2103c6f66eb9555aadb1b2745e35a3b198df5ea0
maguro-alternative <ka.ra.si.naporitan@gmail.com>
2025-08-19 07:42:31 +0900

    大幅書き換え

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,21 @@
-import vercel from '@hono/vite-build'
 import mdx from '@mdx-js/rollup'
-import adapter from '@hono/vite-dev-server/node'
-import tailwindcss from '@tailwindcss/vite'
 import honox from 'honox/vite'
+import client from 'honox/vite/client'
 import { defineConfig } from 'vite'
 import nodeServerPlugin from './vite-node-server-plugin'
 
 export default defineConfig(({ mode }) => {
   if (mode === 'client') {
     return {
-      build: {
-        rollupOptions: {
-          input: ['/app/client.ts', '/app/style.css'],
-          manifest: true,
-        },
-        outDir: './dist/static',
-      },
-      plugins: [tailwindcss()],
+      plugins: [client()],
     }
-  }
-
-  return {
-    plugins: [
-      honox({
-        devServer: {
-          adapter,
-        },
-      }),
-      nodeServerPlugin(),
-      tailwindcss(),
-      mdx({ jsxImportSource: 'hono/jsx' }),
-    ],
+  } else {
+    return {
+      plugins: [
+        honox(),
+        nodeServerPlugin(),
+        mdx({ jsxImportSource: 'hono/jsx' }),
+      ],
+    }
   }
 })
