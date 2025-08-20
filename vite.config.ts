6abce79abbb14db55afdf2ed6afc233031a25f84
maguro-alternative <ka.ra.si.naporitan@gmail.com>
2025-08-20 21:58:10 +0900

    gemini

--- vite.config.ts
+++ vite.config.ts
@@ -1,41 +1,21 @@
-import build from '@hono/vite-build/vercel'
-import mdx from '@mdx-js/rollup'
-import adapter from '@hono/vite-dev-server/node'
-import tailwindcss from '@tailwindcss/vite'
+// 再掲: これが最終的な推奨設定です
+import vercel from '@hono/vite-build'
 import honox from 'honox/vite'
+import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
-import nodeServerPlugin from './vite-node-server-plugin'
 
-export default defineConfig(({ mode }) => {
-  if (mode === 'client') {
-    return {
-      build: {
-        rollupOptions: {
-          input: ['/app/client.ts', '/app/style.css'],
-        },
-        manifest: true,
-        outDir: './dist/static',
-      },
-      plugins: [
-        tailwindcss(),
-        build({
-          entry: './app/client.ts',
-        }),
-      ],
-    }
-  }
-
-  return {
-    plugins: [
-      honox({
-        client: {
-          input: ['/app/client.ts'],
-        },
-      }),
-      tailwindcss(),
-      build({
-        entry: './app/server.ts',
-      })
-    ],
-  }
+export default defineConfig({
+  plugins: [
+    honox({
+      client: {
+        input: [
+          '/app/client.ts'
+        ]
+      }
+    }),
+    tailwindcss(),
+    vercel({
+      entry: './app/server.ts',
+    })
+  ]
 })
