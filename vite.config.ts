addc28e5e267b344a30606bdbf43af29afeb1ce4
maguro-alternative <ka.ra.si.naporitan@gmail.com>
2026-06-06 01:29:28 +0900

    cloudflare用の設定追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
-import build from '@hono/vite-build/vercel'
-import mdx from '@mdx-js/rollup'
+import build from '@hono/vite-build/cloudflare-workers'
 import adapter from '@hono/vite-dev-server/node'
 import tailwindcss from '@tailwindcss/vite'
 import honox from 'honox/vite'
 import { defineConfig } from 'vite'
-import nodeServerPlugin from './vite-node-server-plugin'
-import vercel from 'vite-plugin-vercel';
 
 export default defineConfig(({ mode }) => {
   if (mode === 'client') {
@@ -14,6 +11,7 @@ export default defineConfig(({ mode }) => {
         rollupOptions: {
           input: ['./app/client.ts', './app/style.css'],
         },
+        cssCodeSplit: true,
         manifest: true,
       },
       plugins: [
@@ -29,7 +27,7 @@ export default defineConfig(({ mode }) => {
         client: { input: ['./app/style.css'] }
       }),
       tailwindcss(),
-      vercel()
+      build(),
     ],
   }
 })
